cc0c0756ae2853c667d5f6560e6a86e21a5a5188
stephdin <stephdin@users.noreply.github.com>
2025-10-20 20:51:38 +0200

    vm: add op codes for control flow

--- vm.ts
+++ vm.ts
@@ -1,6 +1,275 @@
+const DEBUG = true;
+
 type Instruction = { op: string; arg?: number };
 
-const program: Array<Instruction> = [
+function run(program: Array<Instruction>) {
+  const stack: Array<number> = [];
+
+  let pc: number = 0;
+  let halted: boolean = false;
+
+  while (!halted && pc < program.length) {
+    const { op, arg } = program[pc];
+    const oldStack = [...stack];
+
+    switch (op) {
+      /* Stack  */
+
+      /* -- x */
+      case "PUSH": {
+        if (typeof arg === "undefined") {
+          throw new Error("No argument given for PUSH");
+        }
+
+        stack.push(arg);
+        break;
+      }
+
+      /* x -- */
+      case "POP": {
+        stack.pop();
+        break;
+      }
+
+      /* x -- x x */
+      case "DUP": {
+        const a = stack.pop();
+
+        if (typeof a === "undefined") {
+          throw new Error("Stack underflow: not enough values to DUP");
+        }
+
+        stack.push(a);
+        stack.push(a);
+        break;
+      }
+
+      /* a b -- b a */
+      case "SWAP": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to SWAP");
+        }
+
+        stack.push(b);
+        stack.push(a);
+        break;
+      }
+
+      /* Arithmetic */
+
+      /* a b -- a+b */
+      case "ADD": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to ADD");
+        }
+
+        stack.push(a + b);
+        break;
+      }
+
+      /* a b -- a-b */
+      case "SUB": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to SUB");
+        }
+
+        stack.push(a - b);
+        break;
+      }
+
+      /* a b -- a*b */
+      case "MUL": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to MUL");
+        }
+
+        stack.push(a * b);
+        break;
+      }
+
+      /* a b -- a/b */
+      case "DIV": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to DIV");
+        }
+
+        stack.push(a / b);
+        break;
+      }
+
+      /* a b -- a%b */
+      case "MOD": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values to MOD");
+        }
+
+        stack.push(a % b);
+        break;
+      }
+
+      /* Logic */
+
+      /* a b -- (a==b ? 1 : 0) */
+      case "EQ": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for EQ");
+        }
+
+        stack.push(a == b ? 1 : 0);
+        break;
+      }
+
+      /* a b -- (a!=b ? 1 : 0) */
+      case "NEQ": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for NEQ");
+        }
+
+        stack.push(a <= b ? 1 : 0);
+        break;
+      }
+
+      /* a b -- (a<b ? 1 : 0) */
+      case "LT": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for LT");
+        }
+
+        stack.push(a < b ? 1 : 0);
+        break;
+      }
+
+      /* a b -- (a<=b ? 1 : 0) */
+      case "LTE": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for LTE");
+        }
+
+        stack.push(a <= b ? 1 : 0);
+        break;
+      }
+
+      /* a b -- (a>b ? 1 : 0) */
+      case "GT": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for GT");
+        }
+
+        stack.push(a > b ? 1 : 0);
+        break;
+      }
+
+      /* a b -- (a>=b ? 1 : 0) */
+      case "GTE": {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        if (typeof a === "undefined" || typeof b === "undefined") {
+          throw new Error("Stack underflow: not enough values for GTE");
+        }
+
+        stack.push(a >= b ? 1 : 0);
+        break;
+      }
+
+      /* I/O */
+
+      /* x -- */
+      case "PRINT": {
+        console.log(stack.pop());
+        break;
+      }
+
+      /* Control Flow */
+
+      case "JMP": {
+        if (typeof arg !== "number") {
+          throw new Error("JMP needs a numeric target address");
+        }
+        pc = arg;
+        continue; // skip program counter increment
+      }
+
+      case "JZ": {
+        if (typeof arg !== "number") {
+          throw new Error("JZ needs a numeric target address");
+        }
+
+        const cond = stack.pop();
+
+        if (cond === 0) {
+          pc = arg;
+          continue;
+        }
+        break;
+      }
+
+      case "JNZ": {
+        if (typeof arg !== "number") {
+          throw new Error("JNZ needs a numeric target address");
+        }
+
+        const cond = stack.pop();
+
+        if (cond !== 0) {
+          pc = arg;
+          continue;
+        }
+        break;
+      }
+
+      case "HALT": {
+        console.log("Halting program");
+        halted = true;
+        break;
+      }
+
+      default:
+        throw new Error("Unknown OP code " + op);
+        break;
+    }
+
+    if (DEBUG) console.log(op, arg ? arg : "", ";", oldStack, "--", stack);
+
+    // increment program counter
+    pc++;
+  }
+}
+
+const program1: Array<Instruction> = [
   { op: "PUSH", arg: 23 },
   { op: "PUSH", arg: 42 },
   { op: "ADD" },
@@ -15,228 +284,24 @@ const program: Array<Instruction> = [
   { op: "HALT" },
 ];
 
-const stack: Array<number> = [];
+const program2: Instruction[] = [
+  { op: "PUSH", arg: 7 },
+  { op: "DUP" },
+  { op: "PRINT" },
+  { op: "PUSH", arg: 1 },
+  { op: "SUB" },
+  { op: "DUP" },
+  { op: "JNZ", arg: 1 },
+  { op: "POP" },
+  { op: "HALT" },
+];
 
-let pc: number = 0;
-let halted: boolean = false;
+const t0 = performance.now();
+run(program1);
+const t1 = performance.now();
+console.log(`running program 1 took ${t1 - t0} milliseconds.`);
 
-while (!halted && pc < program.length) {
-  const { op, arg } = program[pc];
-  const oldStack = [...stack];
-
-  switch (op) {
-    /* Stack  */
-
-    /* -- x */
-    case "PUSH": {
-      if (typeof arg === "undefined") {
-        throw new Error("No argument given for PUSH");
-      }
-
-      stack.push(arg);
-      break;
-    }
-
-    /* x -- */
-    case "POP": {
-      stack.pop();
-      break;
-    }
-
-    /* x -- x x */
-    case "DUP": {
-      const a = stack.pop();
-
-      if (typeof a === "undefined") {
-        throw new Error("Stack underflow: not enough values to DUP");
-      }
-
-      stack.push(a);
-      stack.push(a);
-      break;
-    }
-
-    /* a b -- b a */
-    case "SWAP": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to SWAP");
-      }
-
-      stack.push(b);
-      stack.push(a);
-      break;
-    }
-
-    /* Arithmetic */
-
-    /* a b -- a+b */
-    case "ADD": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to ADD");
-      }
-
-      stack.push(a + b);
-      break;
-    }
-
-    /* a b -- a-b */
-    case "SUB": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to SUB");
-      }
-
-      stack.push(a - b);
-      break;
-    }
-
-    /* a b -- a*b */
-    case "MUL": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to MUL");
-      }
-
-      stack.push(a * b);
-      break;
-    }
-
-    /* a b -- a/b */
-    case "DIV": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to DIV");
-      }
-
-      stack.push(a / b);
-      break;
-    }
-
-    /* a b -- a%b */
-    case "MOD": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values to MOD");
-      }
-
-      stack.push(a % b);
-      break;
-    }
-
-    /* Logic */
-
-    /* a b -- (a==b ? 1 : 0) */
-    case "EQ": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for EQ");
-      }
-
-      stack.push(a == b ? 1 : 0);
-      break;
-    }
-
-    /* a b -- (a!=b ? 1 : 0) */
-    case "NEQ": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for NEQ");
-      }
-
-      stack.push(a <= b ? 1 : 0);
-      break;
-    }
-
-    /* a b -- (a<b ? 1 : 0) */
-    case "LT": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for LT");
-      }
-
-      stack.push(a < b ? 1 : 0);
-      break;
-    }
-
-    /* a b -- (a<=b ? 1 : 0) */
-    case "LTE": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for LTE");
-      }
-
-      stack.push(a <= b ? 1 : 0);
-      break;
-    }
-
-    /* a b -- (a>b ? 1 : 0) */
-    case "GT": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for GT");
-      }
-
-      stack.push(a > b ? 1 : 0);
-      break;
-    }
-
-    /* a b -- (a>=b ? 1 : 0) */
-    case "GTE": {
-      const b = stack.pop();
-      const a = stack.pop();
-
-      if (typeof a === "undefined" || typeof b === "undefined") {
-        throw new Error("Stack underflow: not enough values for GTE");
-      }
-
-      stack.push(a >= b ? 1 : 0);
-      break;
-    }
-
-    /* I/O */
-
-    /* x -- */
-    case "PRINT": {
-      console.log(stack.pop());
-      break;
-    }
-
-    /* Control Flow */
-    case "HALT": {
-      console.log("Halting program");
-      halted = true;
-      break;
-    }
-    default:
-      break;
-  }
-
-  console.log(op, arg ? arg : "", ";", oldStack, "--", stack);
-
-  //increment program counter
-  pc++;
-}
+const t3 = performance.now();
+run(program2);
+const t4 = performance.now();
+console.log(`running program 2 took ${t4 - t3} milliseconds.`);
